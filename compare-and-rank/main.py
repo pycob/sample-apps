e3a5dde906e32af7f581b6f3d7bbc85c530b2f26
Zain Hoda <7146154+zainhoda@users.noreply.github.com>
2023-03-06 13:08:19 -0500

    elo for compare-and-rank

--- compare-and-rank/main.py
+++ compare-and-rank/main.py
@@ -2,88 +2,127 @@
 import pandas as pd
 import random
 import pycob as cob
+from elosports.elo import Elo
 
 # GLOBAL VARIABLES
-df = pd.DataFrame({'name': ['Alice', 'Bob', 'Charlie', 'Dave', 'Eve'],
+example_df = pd.DataFrame({'name': ['Alice', 'Bob', 'Charlie', 'Dave', 'Eve'],
                    'age': [25, 30, 35, 40, 45],
                    'gender': ['female', 'male', 'male', 'male', 'female']})
 
+# Use of global variables works for this example, but is not recommended for production apps
+eloLeague = Elo(k = 20)
+players_df: pd.DataFrame
+index_col: str
+games = []
+
 # PAGE FUNCTIONS
-def index(server_request: cob.Request) -> cob.Page:
+def input(server_request: cob.Request) -> cob.Page:
     page = cob.Page("DataFrame Comparison Game")
-    page.add_header("Welcome to the DataFrame Comparison Game!")
-    page.add_text("Click on the button to start the game")
-    page.add_link("Start Game", "/game")
+    page.add_header("Welcome to the DataFrame Comparison!")
+
+    with page.add_card() as card:
+        with card.add_form(action="/setup", method="POST") as form:
+            form.add_text("Enter your data in JSON format")
+            form.add_formtextarea("Data", "df_str", placeholder="", value=example_df.to_json(orient='records'))
+            form.add_formtext("Index Column", "index_col", placeholder="Which column uniquely identifies the row?", value="name")
+            
+            form.add_text("Click on the button to start the playoffs")
+            form.add_formsubmit("Start Playoffs")
+
     return page
 
-def game(server_request: cob.Request) -> cob.Page:
+def setup(server_request: cob.Request) -> cob.Page:
+    page = cob.Page("DataFrame Comparison Game")
+
+    # Get the dataframe from the form
+    df_str = server_request.params('df_str')
+
+    global index_col
+    index_col = server_request.params('index_col')
+
+    global players_df
+    players_df = pd.read_json(df_str, orient='records')
+
+    # Add the players to the Elo League
+    global eloLeague
+    for index, row in players_df.iterrows():
+        if row[index_col] not in eloLeague.ratingDict:
+            eloLeague.addPlayer(row[index_col])
+
+    page.add_header("Data Saved")
+    page.add_link("Start Playoffs", "/vs")
+
+    return page
+
+
+def row_to_df(row: pd.Series) -> pd.DataFrame:
+    df = pd.DataFrame(row).reset_index()
+    df.columns = ['column', 'value']
+    return df
+
+def vs(server_request: cob.Request) -> cob.Page:
     page = cob.Page("Game")
-    page.add_header("DataFrame Comparison Game")
-    
+    page.add_header("DataFrame Comparison")
+    page.add_text("Select the winner of the following two rows")
+
+    # Get the winner and loser from the query parameters
+    winner = server_request.params('winner')
+    loser = server_request.params('loser')
+
+    # Update the Elo League
+    if winner and loser:
+        games.append({"winner": winner, "loser": loser})
+        eloLeague.gameOver(winner=winner, loser=loser, winnerHome=False)
+
+    global players_df
+    global index_col
+
     # Randomly select 2 rows from the dataframe
-    row1, row2 = random.sample(range(len(df)), 2)
-    data1 = df.iloc[row1]
-    data2 = df.iloc[row2]
+    row1, row2 = random.sample(range(len(players_df)), 2)
+    data1 = players_df.iloc[row1]
+    data2 = players_df.iloc[row2]
     
     # Display the rows and a button to choose the winner
-    with page.add_container(classes="row"):
-        with page.add_container(classes="col-md-6") as container:
-            container.add_header("Row 1")
-            container.add_text(str(data1))
-            container.add_link("Select as Winner", f"/winner?winner={row1}")
-        with page.add_container(classes="col-md-6") as container:
-            container.add_header("Row 2")
-            container.add_text(str(data2))
-            container.add_link("Select as Winner", f"/winner?winner={row2}")
+    with page.add_container(grid_columns=3) as container:
+        with container.add_card() as card:
+            card.add_header(str(data1[index_col]))
+            card.add_pandastable(row_to_df(data1), action_buttons=[])
+            card.add_link("Select as Winner", f"/vs?winner={data1[index_col]}&loser={data2[index_col]}")
+        
+        container.add_header("VS", classes="align-self-center text-center")
+
+        with container.add_card() as card:
+            card.add_header(str(data2[index_col]))
+            card.add_pandastable(row_to_df(data2), action_buttons=[])
+            card.add_link("Select as Winner", f"/vs?winner={data1[index_col]}&loser={data2[index_col]}")
     
+    page.add_link("See Results", "/results")
+
     return page
-    
-def winner(server_request: cob.Request) -> cob.Page:
-    page = cob.Page("Winner")
-    page.add_header("DataFrame Comparison Game")
-    
-    winner = int(server_request.params('winner'))
-    
-    # Record the winner in a separate dataframe
-    if 'winners' not in server_request.session:
-        server_request.session['winners'] = pd.DataFrame(columns=['name', 'age', 'gender', 'winner'])
-    winners_df = server_request.session['winners']
-    winners_df = winners_df.append(df.iloc[winner].append(pd.Series({'winner': winner})), ignore_index=True)
-    server_request.session['winners'] = winners_df
-    
-    page.add_text(f"You selected {df.iloc[winner]['name']} as the winner!")
-    page.add_button("Play Again", "/game")
-    page.add_button("View Results", "/results")
-    
-    return page
-    
+        
 def results(server_request: cob.Request) -> cob.Page:
     page = cob.Page("Results")
-    page.add_header("DataFrame Comparison Game")
-    
-    # Display the winners dataframe
-    if 'winners' in server_request.session:
-        winners_df = server_request.session['winners']
-        with page.add_container(classes="row"):
-            with page.add_container(classes="col-md-8 offset-md-2") as container:
-                container.add_header("Winners")
-                container.add_dataframe(winners_df)
-    else:
-        page.add_text("No winners yet!")
-    
-    page.add_button("Play Again", "/game")
-    page.add_button("Home", "/")
-    
+
+    page.add_header("Rankings")
+    rating_df = pd.DataFrame(pd.Series(eloLeague.ratingDict, name="Rating")).sort_values("Rating", ascending=False).reset_index()
+    rating_df.columns = ['Player', 'Rating']
+
+    page.add_pandastable(rating_df, action_buttons=[])
+
+    page.add_header("Games")
+    games_df = pd.DataFrame(games)
+    page.add_pandastable(games_df, action_buttons=[])
+
     return page
     
 # APP CONFIGURATION
 
-app = cob.App("DataFrame Comparison Game", use_built_in_auth=False)
+app = cob.App("Compare and Rank", use_built_in_auth=False)
 
-app.add_page('/', 'index', index, show_in_navbar=True, footer_category=None)
-app.add_page('/game', 'game', game, show_in_navbar=False, footer_category=None)
-app.add_page('/winner', 'winner', winner, show_in_navbar=False, footer_category=None)
-app.add_page('/results', 'results', results, show_in_navbar=True, footer_category=None)
+app.register_function(input, show_in_navbar=False)
+app.register_function(setup, show_in_navbar=False, footer_category=None)
+app.register_function(vs, show_in_navbar=False, footer_category=None)
+app.register_function(results, show_in_navbar=True)
 
 server = app.run()
 # Run this using `python3 main.py` or `python main.py` depending on your system.
