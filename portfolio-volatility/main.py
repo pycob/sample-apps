b62ef51c2a3fea3c67e5893d1e0403eb09f98cfc
Zain Hoda <7146154+zainhoda@users.noreply.github.com>
2023-03-08 15:06:16 -0500

    update portfolio-volatility

--- portfolio-volatility/main.py
+++ portfolio-volatility/main.py
@@ -3,78 +3,63 @@ import pandas as pd
 import plotly.express as px
 import snowflake.connector
 
-def get_secret(app: cob.App, secret_name: str) -> str:
-    secrets = app.retrieve_dict(table_id="secrets", object_id="secrets")
-    if secrets is None:
-        raise ValueError("Secrets not found")
-    
-    if secret_name in secrets:
-        return secrets[secret_name]
-    else:
-        raise ValueError(f"Secret {secret_name} not found")
+app = cob.App("Portfolio Volatility")
+
+initial_tickers_and_weights = {
+    "AAPL": 0.2,
+    "MSFT": 0.2,
+    "AMZN": 0.2,
+    "GOOG": 0.2,
+    "FB": 0.2,
+}
 
 def get_data(app: cob.App, tickers: list) -> pd.DataFrame:
     """Get the data for the tickers"""
     # Get the data
     conn = snowflake.connector.connect(
-        user=get_secret(app, "SNOWFLAKE_USER"),
-        password=get_secret(app, "SNOWFLAKE_PASSWORD"),
-        account=get_secret(app, "SNOWFLAKE_ACCOUNT"),
+        user=app.retrieve_secret("SNOWFLAKE_USER"),
+        password=app.retrieve_secret("SNOWFLAKE_PASSWORD"),
+        account=app.retrieve_secret("SNOWFLAKE_ACCOUNT"),
         )
     cs = conn.cursor()
 
-    cur = cs.execute("""
+    # Load data for tickers
+    cur = cs.execute(f"""
     SELECT "ticker", "date", "closeadj"
     FROM QUANDL.TYPED.SEP
-    WHERE "ticker" in ('AAPL', 'MSFT', 'AMZN', 'GOOG', 'FB')
+    WHERE "ticker" in ({', '.join([f"'{ticker}'" for ticker in tickers])})
     ORDER BY "date"
-    """)    
+    """)
 
     data = cur.fetch_pandas_all()
 
-    # Calculate the returns
+    return data
+
+def update_tickers_and_weights(before: dict, ticker: str, weight: float) -> dict:
+    after = before.copy()
+    after[ticker] = weight
+
+    # Normalize the weights
+    total_weight = sum(after.values())
+
+    for ticker in after:
+        after[ticker] = after[ticker] / total_weight
+
+    return after
+
+def compute_daily_returns(data: pd.DataFrame) -> pd.DataFrame:
     data["return"] = data.groupby("ticker")["closeadj"].pct_change()
 
     return data
 
-# Page Functions
-def home(server_request: cob.Request) -> cob.Page:
-    page = cob.Page("Portfolio Volatility")
-
-    tickers_and_weights = {
-        "AAPL": 0.2,
-        "MSFT": 0.2,
-        "AMZN": 0.2,
-        "GOOG": 0.2,
-        "FB": 0.2,        
-    }
-
-    # Get the data
-    data = get_data(server_request.app, tickers_and_weights.keys())
-
+def compute_portfolio_returns(data: pd.DataFrame, tickers_and_weights: dict) -> pd.DataFrame:
     data['weighted_return'] = data['return'] * data['ticker'].map(tickers_and_weights)
 
-    page.add_pandastable(data.head())
+    portfolio_return = data.groupby("date")["weighted_return"].sum()
 
-    # Calculate the portfolio volatility
-    portfolio_volatility = data.groupby("date")["weighted_return"].std().mean() * 252 ** 0.5
+    return portfolio_return
 
-    page.add_text(f"Portfolio Volatility: {portfolio_volatility}")
-
-    # Plotly Histogram of the returns
-    fig = px.histogram(data, x="return", nbins=100, title="Returns")
-    page.add_plotlyfigure(fig)
-
-    # Compute portfolio Value at Risk
-    portfolio_var = data.groupby("date")["weighted_return"].quantile(0.025).mean() * 252 ** 0.5
-
-    page.add_text(f"Portfolio Value at Risk: {portfolio_var}")
-
-    # Compute portfolio Expected Shortfall
-    portfolio_es = data.groupby("date")["weighted_return"].apply(lambda x: x[x < x.quantile(0.025)].mean()).mean() * 252 ** 0.5
-
-    page.add_text(f"Portfolio Expected Shortfall: {portfolio_es}")
-
+def get_return_vs_volatility(data: pd.DataFrame) -> pd.DataFrame:
     # First Date
     first_date = data["date"].min()
 
@@ -90,20 +75,89 @@ def home(server_request: cob.Request) -> cob.Page:
     # Join ticker_volatility and total_returns
     return_vs_volatility = total_returns.to_frame("total_return").join(ticker_volatility.to_frame("volatility"))
 
-    page.add_pandastable(return_vs_volatility.reset_index())
+    return return_vs_volatility
+
+# Page Functions
+def home(server_request: cob.Request) -> cob.Page:
+    page = cob.Page("Portfolio Volatility")
+
+    ticker = server_request.params("ticker")
+    weight = server_request.params("weight")
+
+    if ticker is not None and weight is not None:
+        # Update the tickers and weights
+        tickers_and_weights = update_tickers_and_weights(initial_tickers_and_weights, ticker, float(weight))
+    else:
+        tickers_and_weights = initial_tickers_and_weights
+
+    # Get the data
+    data = get_data(server_request.app, tickers_and_weights.keys())
+    data = compute_daily_returns(data)
+    portfolio_return = compute_portfolio_returns(data, tickers_and_weights)
+
+    # Calculate the portfolio volatility
+    portfolio_volatility = portfolio_return.std() * 252 ** 0.5
+
+    # Compute the portfolio cumulative return
+    portfolio_cumulative_return = (1+portfolio_return).cumprod()-1
+
+    # Compute portfolio Value at Risk
+    tail_cutoff = data.groupby("date")["weighted_return"].sum().quantile(0.025)
+    portfolio_var = tail_cutoff * 252 ** 0.5
+
+    # Compute portfolio Expected Shortfall
+    portfolio_es = portfolio_return[portfolio_return < tail_cutoff].mean() * 252 ** 0.5
+
+    # Plotly Histogram of the returns
+    fig1 = px.histogram(portfolio_return, x="weighted_return", nbins=100, title="<b>Daily Portfolio Return Distribution<b>")
+    fig1.layout.xaxis.tickformat = ',.0%'
+    
+    # Plotly Cumulative Return
+    fig2 = px.line(pd.DataFrame(portfolio_cumulative_return), x=portfolio_cumulative_return.index, y="weighted_return", title="<b>Portfolio Cumulative Return</b>")
+    fig2.layout.yaxis.tickformat = ',.0%'
 
     # Plotly Scatter of the returns vs volatility
-    fig = px.scatter(return_vs_volatility.reset_index(), x="volatility", y="total_return", title="Returns vs Volatility", hover_name="ticker")
-    page.add_plotlyfigure(fig)
+    return_vs_volatility = get_return_vs_volatility(data)
+    fig3 = px.scatter(return_vs_volatility.reset_index(), x="volatility", y="total_return", title="<b>Returns vs Volatility</b>", text="ticker")
+    fig3.update_traces(textposition="bottom right")
 
     # Compute the correlation matrix
     correlation_matrix = data.pivot(index="date", columns="ticker", values="return").corr()
 
-    page.add_pandastable(correlation_matrix.reset_index())
+    # Plotly Heatmap of the correlation matrix
+    fig4 = px.imshow(correlation_matrix, title="<b>Correlation Matrix</b>")
+
+    with page.add_container(grid_columns=3) as risk_stats:
+        with risk_stats.add_card() as card:
+            card.add_header(f"{portfolio_volatility:.2%}")
+            card.add_text("Portfolio Volatility")
+        
+        with risk_stats.add_card() as card:
+            card.add_header(f"{portfolio_var:.2%}")
+            card.add_text("Portfolio Value at Risk")
+
+        with risk_stats.add_card() as card:
+            card.add_header(f"{portfolio_es:.2%}")
+            card.add_text("Portfolio Expected Shortfall")
+
+    with page.add_container(grid_columns=2) as plots1:
+        plots1.add_plotlyfigure(fig1)
+        plots1.add_plotlyfigure(fig2)
+    
+    with page.add_container(grid_columns=2) as plots2:
+        plots2.add_plotlyfigure(fig3)
+        plots2.add_plotlyfigure(fig4)
+
+    with page.add_card() as card:
+        card.add_header("Add Ticker")
+        card.add_text("The rest of the portfolio will be rebalanced to maintain the relative weights.")
+        with card.add_form() as form:
+            form.add_formtext("Ticker", "ticker", "AAPL")
+            form.add_formtext("Weight", "weight", "0.2")
+            form.add_formsubmit("Add Ticker")
 
     return page
 
-app = cob.App("Portfolio Volatility")
 
 # Add the pages
 app.register_function(home)
